b24a06b05ed43ffd94f01cbb8c83b5ee1b3e53e5
professordavidx <professordavidgomez@gmail.com>
2026-03-07 13:13:41 -0500

    Picture Album completed

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+// Dynamic Year and Last Modified
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent =
+  `Last Modification: ${document.lastModified}`;
+
+// Hamburger Menu Toggle
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('open');
+});
